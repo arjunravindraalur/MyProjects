bedb15be107c57a5393b5acda3e9ea92ebc4c2fb
Arjun Ravindra <arjunravindraaluranuradhakashy@gmail.com>
2025-09-03 23:13:06 +0530

    python data types and variables

---
+++ main.py
@@ -0,0 +1,34 @@
+x=5
+name="Welcome"
+pi=3.14
+is_active=True
+c=2+3j
+z=3+6j
+d=[1,2,3]
+e=(1,2,3)
+f={"a","b","c"}
+g=None
+thisdict={"brand":"Ford","model":"mustang","year":1995}
+
+h=range(6)
+for i in range (x):
+    print(i)
+
+
+print(type(x))
+print(type(name))
+print(type(pi))
+print(type(is_active))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(thisdict))
+print(type(z))
+print("The type of x is",type(x))
+print("The type of name is",type(name))
+print("The type of pi is",type(pi))
+
+
+
