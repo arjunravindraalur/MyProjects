7b5653c6aaef1bc8ed63dada5dbda703c0c53af0
Arjun Ravindra <arjunravindraaluranuradhakashy@gmail.com>
2025-09-16 12:20:52 +0530

    Intial commit

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
-x=5
-name="Welcome"
-pi=3.14
-is_active=True
-c=2+3j
-z=3+6j
-d=[1,2,3]
-e=(1,2,3)
-f={"a","b","c"}
-g=None
-thisdict={"brand":"Ford","model":"mustang","year":1995}
+a=42
+b="Hello Python"
+c=100
+d=3.1423
+e=2+4j
+f=[1,2,3,"apple",5.7]
+g=(10,20,30,"apple")
+h={1,2,3,3,4,5}
+i={"name":"arjun","age":40,"language":"java"}
+j=range(7)
+k=frozenset([1,2,3,4])
+l=True
+m=b"Hello"
+n=bytearray([1,2,3,4,5])
+o=memoryview(n)
+p=None
+q=complex(6,9)
 
-h=range(6)
-for i in range (x):
-    print(i)
+
+list1=[7,3.4,[-6,4],["apple","grapes"]]
+
+tuple1= (("parrot","sparrow"),("Lion","tiger"))
+
+print(tuple1)
+print("the type of tuple1 is",type(tuple1))
 
 
-print(type(x))
-print(type(name))
-print(type(pi))
-print(type(is_active))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
-print(type(thisdict))
-print(type(z))
-print("The type of x is",type(x))
-print("The type of name is",type(name))
-print("The type of pi is",type(pi))
+print(list1)
+print("the type of list1 is", type(list1))
+
+
+x = range(6)
+
+for y in x:
+  print(y)
+
+
+
+print("the type of a is", type(a))
+print("the type of b is", type(b))
+print("the type of c is", type(c))
+print("the type of d is", type(d))
+print("the type of e is", type(e))
+print("the type of f is", type(f))
+print("the type of g is", type(g))
+print("the type of h is", type(h))
+print("the type of i is", type(i))
+print("the type of j is", type(j))
+print("the type of k is", type(k))
+print("the type of l is", type(l))
+print("the type of m is", type(m))
+print("the type of n is", type(n))
+print("the type of o is", type(o))
+print("the type of p is", type(p))
+print("the type of q is", type(q))
+
 
 
 
